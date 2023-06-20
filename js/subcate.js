fb54bbe77636ce33640e9ab3e703a8c197d8a82f
Namanqw1 <nasuchacha1708@gmail.com>
2023-06-20 10:57:48 +0530

    top nav bar completed

--- js/subcate.js
+++ js/subcate.js
@@ -1,9 +1,18 @@
-function subcate(elementId) {
-    var cateagor = document.getElementById(elementId);
-    cateagor.classList.add('active');
-}
+const subnav = document.querySelectorAll('.Mcate1');
 
-function subcateout(elementId) {
-    var cateagor = document.getElementById(elementId);
-    cateagor.classList.remove('active');
-}
+subnav.forEach((Mcate1) => {
+      const targetId = Mcate1.dataset.target;
+      const cate1 = document.getElementById(targetId);
+
+    Mcate1.addEventListener('mouseenter', () => {
+      cate1.classList.remove('animate__fadeOutUp');
+      cate1.classList.add('animate__fadeInDown');
+      cate1.style.display = 'block';
+    });
+  
+    Mcate1.addEventListener('mouseleave', () => {
+      cate1.classList.remove('animate__fadeInDown');
+      cate1.style.display = 'none';
+    });
+
+  });
